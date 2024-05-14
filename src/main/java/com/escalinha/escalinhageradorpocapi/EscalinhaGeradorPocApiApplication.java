14bf58405d9b92b049e85ad8accb5cb0738aebd1
Marcos Esmeraldino <marcosesmeraldino@gmail.com>
2024-05-14 19:11:30 -0300

    feat: setup

---
+++ src/main/java/com/escalinha/escalinhageradorpocapi/EscalinhaGeradorPocApiApplication.java
@@ -0,0 +1,13 @@
+package com.escalinha.escalinhageradorpocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalinhaGeradorPocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalinhaGeradorPocApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escalinha/escalinhageradorpocapi/EscalinhaGeradorPocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escalinha.escalinhageradorpocapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalinhaGeradorPocApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
