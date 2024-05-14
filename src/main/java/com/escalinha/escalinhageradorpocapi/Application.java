b496bbf1fd220f054d4507d55890cb53ba9239c4
Marcos Andre Esmeraldino Rocha <marcos_arocha@sicredi.com.br>
2024-05-14 19:43:59 -0300

    feat: setup

--- src/main/java/com/escalinha/escalinhageradorpocapi/Application.java
+++ src/main/java/com/escalinha/escalinhageradorpocapi/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EscalinhaGeradorPocApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EscalinhaGeradorPocApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
